f2206a2af152dfd1cc374dbf7316412d7334fb30
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2024-06-01 11:16:21 +0530

    Add files via upload

---
+++ webx/script.lua
@@ -0,0 +1,16 @@
+local headers = {
+    ["Authorization"] = "Bearer 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"
+}
+
+function query(messages)
+    local response = fetch({
+        url = "https://api-inference.huggingface.co/models/microsoft/Phi-3-mini-4k-instruct",
+        method = "GET",
+        headers = headers,
+        body = '{ "inputs": ' .. messages .. '}'
+    })
+
+print()
+get("input").on_submit(function(content)
+    print(query(content))
+end)
